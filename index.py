ad28f172e881237f45bb4a11bdd39ea90679828b
starslilia <anajuliapinheiro06@gmail.com>
2023-10-27 11:10:25 -0300

    eba

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
+from templates.manterclienteUI import ManterClienteUI
+from templates.manterservicoUI import ManterServicoUI
+from templates.manteragendaUI import ManterAgendaUI
+from templates.abriragendaUI import AbrirAgendaUI
+
 import streamlit as st
-from manterclienteUI import ManterClienteUI
 
 class IndexUI:
-    @classmethod
-    def main(cls):
-      #InicioUI.main()
-      #EquacaoUI.main()
-      ManterClienteUI.main()
+      
+    def sidebar():
+      op = st.sidebar.selectbox("Menu", ["Manter Clientes", "Manter Serviços", "Manter Agenda", "Abrir Agenda do Dia"])
+      if op == "Manter Clientes": ManterClienteUI.main()
+      if op == "Manter Serviços": ManterServicoUI.main()
+      if op == "Manter Agenda": ManterAgendaUI.main()
+      if op == "Abrir Agenda do Dia": AbrirAgendaUI.main()
+
+      #if op == "Manter Clientes": st.session_state["page"] = "manter_clienteUI"
+
+    def main():
+      IndexUI.sidebar()
+
+      #if "page" not in st.session_state: st.session_state["page"] = "equacaoUI"
+      #if st.session_state["page"] == "manter_clienteUI": ManterClienteUI.main()
 
 IndexUI.main()
+
+
+
--- models/cliente.py
+++ models/cliente.py
@@ -6,71 +6,82 @@ class Cliente:
     self.__nome = nome
     self.__email = email
     self.__fone = fone
+
   def get_id(self): return self.__id
   def get_nome(self): return self.__nome
   def get_email(self): return self.__email
   def get_fone(self): return self.__fone
+
   def set_id(self, id): self.__id = id
   def set_nome(self, nome): self.__nome = nome
   def set_email(self, email): self.__email = email
   def set_fone(self, fone): self.__fone = fone
+
   def __eq__(self, x):
     if self.__id == x.__id and self.__nome == x.__nome and self.__email == x.__email and self.__fone == x.__fone:
       return True
-    return False 
+    return False
+
   def __str__(self):
     return f"{self.__id} - {self.__nome} - {self.__email} - {self.__fone}"
 
+
 class NCliente:
-  __clientes = []         # lista de clientes inicia vazia
+  __clientes = []  # lista de clientes inicia vazia
+
   @classmethod
   def inserir(cls, obj):
-    NCliente.abrir()
-    id = 0 # encontrar o maior id já usado
-    for cliente in cls.__clientes:
-      if cliente.get_id() > id: id = cliente.get_id()
+    cls.abrir()
+    id = 0  # encontrar o maior id já usado
+    for aux in cls.__clientes:
+      if aux.get_id() > id: id = aux.get_id()
     obj.set_id(id + 1)
     cls.__clientes.append(obj)  # insere um cliente (obj) na lista
-    NCliente.salvar()
+    cls.salvar()
+
   @classmethod
   def listar(cls):
-    NCliente.abrir()    
-    return cls.__clientes       # retorna a lista de clientes
+    cls.abrir()
+    return cls.__clientes  # retorna a lista de clientes
+
   @classmethod
   def listar_id(cls, id):
-    NCliente.abrir()
-    for cliente in cls.__clientes:
-      if cliente.get_id() == id: return cliente
+    cls.abrir()
+    for obj in cls.__clientes:
+      if obj.get_id() == id: return obj
     return None
+
   @classmethod
   def atualizar(cls, obj):
-    NCliente.abrir()
-    cliente = cls.listar_id(obj.get_id())
-    if cliente is not None:
-      cliente.set_nome(obj.get_nome())
-      cliente.set_email(obj.get_email())
-      cliente.set_fone(obj.get_fone())
-      NCliente.salvar()
+    cls.abrir()
+    aux = cls.listar_id(obj.get_id())
+    if aux is not None:
+      aux.set_nome(obj.get_nome())
+      aux.set_email(obj.get_email())
+      aux.set_fone(obj.get_fone())
+      cls.salvar()
+
   @classmethod
   def excluir(cls, obj):
-    NCliente.abrir()
-    if cliente is not None:
-      cliente = cls.listar_id(obj.get_id())
-      cls.__clientes.remove(cliente)    
-      NCliente.salvar()
+    cls.abrir()
+    aux = cls.listar_id(obj.get_id())
+    if aux is not None:
+      cls.__clientes.remove(aux)
+      cls.salvar()
+
   @classmethod
   def abrir(cls):
+    cls.__clientes = []
     try:
-      cls.__clientes = []
-      with open("clientes.json", mode="r") as f:
-        s = json.load(f)
-        for cliente in s:
-          c = Cliente(cliente["_Cliente__id"], cliente["_Cliente__nome"],
-                     cliente["_Cliente__email"], cliente["_Cliente__fone"])
-          cls.__clientes.append(c)
+      with open("clientes.json", mode="r") as arquivo:
+        clientes_json = json.load(arquivo)
+        for obj in clientes_json:
+          aux = Cliente(obj["_Cliente__id"], obj["_Cliente__nome"], obj["_Cliente__email"], obj["_Cliente__fone"])
+          cls.__clientes.append(aux)
     except FileNotFoundError:
       pass
+
   @classmethod
   def salvar(cls):
-    with open("clientes.json", mode="w") as f:
-      json.dump(cls.__clientes, f, default=vars, indent=4)
+    with open("clientes.json", mode="w") as arquivo:
+      json.dump(cls.__clientes, arquivo, default=vars)
